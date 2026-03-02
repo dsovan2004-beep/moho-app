ff46f73ca7b28a3c695ebd524bace8bf060fa276
dsovan2004-beep <dsovan2004@gmail.com>
2026-03-02 11:02:56 -0800

    Update next.config.js from scaffold

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Required for @cloudflare/next-on-pages
+  // See: https://developers.cloudflare.com/pages/framework-guides/nextjs/
   images: {
+    // Use Cloudflare Image Resizing instead of Next.js built-in image optimisation
+    // (not supported in the Cloudflare Workers runtime)
+    loader: 'custom',
+    loaderFile: './image-loader.js',
     remotePatterns: [
       {
         protocol: 'https',
