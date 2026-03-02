38b8df3ef3a89d87ca8ff6bfa2dcce5dc0cd0abf
dsovan2004-beep <dsovan2004@gmail.com>
2026-03-02 11:02:46 -0800

    Update app/layout.tsx from scaffold

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const runtime = 'edge'
+
 import Link from 'next/link'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { useState, Suspense } from 'react'
